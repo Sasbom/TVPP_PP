4bcf5c44864d597ca5871cb9c8f67d4b11745b53
Sasbom <saskervg@live.nl>
2025-05-12 16:47:24 +0200

    added number parsing to num util

--- TVPP_PP/tvp_pp/file_util.hpp
+++ TVPP_PP/tvp_pp/file_util.hpp
@@ -6,6 +6,50 @@
 #include <cstdint>
 #include "../mio/single_include/mio/mio.hpp"
 #include "num_util.hpp"
+#include <string>
+
+struct FileInfo {
+	std::size_t width;
+	std::size_t height;
+	double fps;
+	double pix_aspect_ratio;
+	bool field_order;
+	std::size_t start_frame;
+	std::string uid;
+	std::size_t shots;
+	std::size_t drawings;
+	std::string info;
+	std::vector<std::string> notes;
+	std::string author;
+	std::string build; // date of build
+	std::string host; // software name
+	std::string os;
+	std::string date_creation;
+	std::string date_lastchanged;
+
+	std::size_t camera_width;
+	std::size_t camera_height;
+	unsigned short camera_field_order;
+	double camera_pix_aspect_ratio;
+	bool camera_aa;
+	bool camera;
+	bool camera_show43_border;
+	bool camera_safearea;
+	double camera_safearea_borderout;
+	double camera_safearea_borderin;
+	
+	bool is_locked;
+	bool is_protected;
+	std::string password;
+	
+	bool save_audio_deps;
+	bool save_video_deps;
+	std::vector<std::string> save_history;
+	double user_workduration;
+	std::string user_workchange; // don't know about this one yet.
+};
+
+
 
 // unknown sentinel info BC 40 11 D7 | �@�
 
@@ -39,7 +83,7 @@ std::span<std::uint8_t const> seek_header(mio::ummap_source& mmap_file, std::siz
 	return std::span<std::uint8_t const>(it, it + length);
 }
 
-void file_read_header(std::span<std::uint8_t const>& header_keyvalue_section) {
+std::vector<std::string> file_read_header(std::span<std::uint8_t const>& header_keyvalue_section) {
 	std::vector<std::string> strings{};
 	
 	std::u16string collect{};
@@ -53,7 +97,9 @@ void file_read_header(std::span<std::uint8_t const>& header_keyvalue_section) {
 			collect.clear();
 		}
 	}
-	for (auto s : strings) {
-		std::cout << s << "\n";
-	}
+	return strings;
+}
+
+FileInfo parse_header_into_fileinfo(std::vector<std::string>& headerinfo) {
+	auto fi = FileInfo{};
 }
